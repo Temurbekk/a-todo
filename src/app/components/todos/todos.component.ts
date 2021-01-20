2250505b46350a8d610b33fb48d9bc4ed5e7d989
Temurbek Sabirov <prodota150@gmail.com>
2021-01-19 19:28:03 -0500

    Inject service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Todo } from '../../models/Todo';
-
+import { TodoService } from '../../services/todo.service';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -10,25 +10,9 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos: Todo[] | undefined;
 
-  constructor() {}
+  constructor(private _todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false,
-      },
-    ];
+    this.todos = this._todoService.getTodos();
   }
 }
