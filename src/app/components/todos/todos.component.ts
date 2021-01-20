e8450f7f2e58fdb2b62eabb2ade30a4774d53814
Temurbek Sabirov <prodota150@gmail.com>
2021-01-19 19:01:21 -0500

    Link todo

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from '../../models/Todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false,
+      },
+    ];
   }
-
 }
